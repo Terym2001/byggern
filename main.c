154a06dc40a39abe75534b73b55d3cfdbf208373
Owen <>
2024-09-05 14:09:29 +0200

    Revised blink led

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <avr/io.h>
-#include <utils/delay.h>
+#include <util/delay.h>
+
+#define FCPU 4915200
 
 int main(void)
 {
+  DDRA |= (1 << PD0); 
+
+  while(1)
+  {
+    PORTA |= (1 << PD0);
+    _delay_ms(200);
+    PORTA &= ~(1 << PD0);
+    _delay_ms(200);
+  }
   return 0;
 }
