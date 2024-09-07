f8e2732b00e686d57c752ee19bffb151f4e5fe97
Terym2001 <tadyrkor@gmail.com>
2024-09-07 20:05:26 +0200

    USART init now links usart to printf (Kanskje?)

--- usart.c
+++ usart.c
@@ -9,23 +9,26 @@ void USART_Init(unsigned int ubrr)
   // Enable transmitter and receiver
   UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 
- /* Set frame format: 8data, 2stop bit */
+  /* Set frame format: 8data, 2stop bit */
   UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);
+
+  // Link USART to printf function in stdio.h
+  fdevopen(&USART_Transmit, &USART_Receive);
 
   return;
 }
 
-void USART_Transmit(unsigned char data )
+int USART_Transmit(unsigned char data, FILE *file)
 {
   //Wait for empty transmit buffer( UDRE0 flag is set when ready )
   while( !( UCSR0A & (1<<UDRE0)) );
 
   //Put the data in the buffer, sends data
   UDR0 = data;
-  return;
+  return 0;
 }
 
-unsigned char USART_Receive( void )
+int USART_Receive(FILE *file)
 {
   //Wait for data to be received 
   while( !(UCSR0A & RXC0) );
