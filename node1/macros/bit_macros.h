30fd98218d8e970b00245901609e08505fafcc1b
Owen <>
2024-09-12 12:36:03 +0200

    Created gitignore file, removed bitmacros

--- node1/macros/bit_macros.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BIT_MACROS_H
-#define _BIT_MACROS_H
-
-#define SET_BIT(reg, bit)     (reg |= (1 << bit))
-#define CLEAR_BIT(reg, bit)   (reg &= ~(1 << bit))
-#define TOGGLE_BIT(reg, bit)  (reg ^= (1 << bit))
-
-#endif
