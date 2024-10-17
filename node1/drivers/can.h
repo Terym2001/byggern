8021eb07aa540581a70337d2239ca8da87eb318a
Terym2001 <tadyrkor@gmail.com>
2024-10-17 12:31:02 +0200

    changed dir name to drivers/

--- node1/drivers/can.h
+++ node1/drivers/can.h
@@ -12,7 +12,7 @@ struct can_message {
 
 void CAN_Init(void);
 
-struct can_message CAN_Recieve();//TODO: IMPLMENT THIS WITH IDS and stuff
+struct can_message CAN_Recieve();
 
 void CAN_Send(struct can_message* msg, uint8_t msg_priority, uint8_t txBuffer);
 
