5cbaf2cd360104578f6c21ba8fb6fd532a42c4fd
Terym2001 <tadyrkor@gmail.com>
2024-11-01 10:20:12 +0100

    working pwm

--- node2/drivers/pwm.c
+++ node2/drivers/pwm.c
@@ -2,71 +2,49 @@
 
 void pwm_init(void)
 {
-  PMC->PMC_WPMR &= ~(2 << 0);
-  PMC->PMC_PCER0 = (PMC_PCER0_PID27);
+  PMC->PMC_WPMR = (0x504D43 << 8);
+  PMC->PMC_WPMR &= ~(1 << 0);
   PMC->PMC_PCER1 = (PMC_PCER1_PID36);
-  PMC->PMC_WPMR |= ~(2 << 0);
-
-  // Unlock user interface (value equals "PWM" in ASCII)
-  PWM->PWM_WPCR = 0x50574D;
+  // TODO: either 1 or 0
+  PMC->PMC_PCER0 |= (1 << 12);
 
   // TODO: Double check
   // Initialize peripheral clock
-  PMC->PMC_PCER0 |= (1 << 13);
   PIOB->PIO_ABSR |= (1 << 13);
-  PIOB->PIO_OER |= (1 << 13);    // Enable output on the pin
   PIOB->PIO_PDR |= (1 << 13);    // Enable the pin
   PIOB->PIO_MDDR |= (1 << 13);   // Disable the multi-driver
+
+  // Unlock user interface (value equals "PWM" in ASCII)
+  PWM->PWM_WPCR = (0x50574D << 8) | (0b11111 << 2);
+  PWM->PWM_WPCR &= ~((1 << 0) | (1 << 1));
 
   // Set prescaler for PWM register b clock to 128 and div to 1
   //PWM->PWM_CLK |= (0b0111 << 24) | (1 << 16);
 
   // Select clock B for channel 1
-  PWM->PWM_CH_NUM[1].PWM_CMR |= (0b0111);
+  PWM->PWM_CH_NUM[1].PWM_CMR |= (0b0111) | (1 << 9);
   //PWM->PWM_CH_NUM[1].PWM_CMR |= (1 << 3) | (1 << 2);
   //PWM->PWM_CH_NUM[1].PWM_CMR &= ~(0b11 << 0);
-
-  // Configuration of the waveform alignment
-  PWM->PWM_CH_NUM[1].PWM_CMR &= ~(1 << 8);
-
-  // Configuration of the output waveform polarity 
-  PWM->PWM_CH_NUM[1].PWM_CMR |= (1 << 9);
-
-  // Selection of the counter event selection
-  PWM->PWM_CH_NUM[1].PWM_CMR &= ~(1 << 10);
-
+  
   // Configuration of the period
   PWM->PWM_CH_NUM[1].PWM_CPRD = CALCULATED_CPRD;
 
   // Configuration of the duty-cycle
-  PWM->PWM_CH_NUM[1].PWM_CDTY = (float) 0.0015;
+  PWM->PWM_CH_NUM[1].PWM_CDTY = (75 * CALCULATED_CPRD) / 1000;
 
-  // TODO: find out what it does
-  // Configuration of the dead-time generator
+  // Configuration of the waveform alignment
+  //PWM->PWM_CH_NUM[1].PWM_CMR &= ~(1 << 8);
 
-  // Selection of the synchronous channels
-  // PWM->PWM_SCM &= ~(1 << 1);
+  // Configuration of the output waveform polarity 
+  //PWM->PWM_CH_NUM[1].PWM_CMR |= (1 << 9);
 
-  // TODO: Interrupt???
-  // Selection of the moment when the WRDY flag and the corresponding PDC transfer request are set
-  
-  // Configuration of the update mode
-  // PWM->PWM_SCM &= ~(0 << 16);
-  // PWM->PWM_SCM &= ~(0 << 17);
+  // Selection of the counter event selection
+  PWM->PWM_CH_NUM[1].PWM_CMR &= ~(1 << 10);
 
-  // TODO: Dont think we need as its not sync
-  // Configuration of the update period
-
-  // TODO: Might not need
-  // Configuration of the comparisons PC
-  
-  // TODO: Might need Configuration of the fault inputs polarity 
-
-  // TODO: Configuration of the fault protection 
-  
-  // TODO: Enable of the Interrupts?!--><--
 
   // Enable of the PWM channels
   PWM->PWM_ENA |= (1 << 1);
+
+  PWM->PWM_WPCR |= (1 << 0) | (1 << 1);
   return;
 }
